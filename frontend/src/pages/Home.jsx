2f7d5161c7bc5b5799e690b5d1fd7bd58d9ddfdd
shreekrishna <shreekrishnaudupa@gmail.com>
2026-01-23 04:00:05 +0530

    Initiased the landing page for users

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import DeploymentDiagram from '../../assets/deployment.jpeg';
+
 export default function Home () {
   return (
     <>
-      This is the Homepage (CI/CD testing 3)
+    <br></br>
+      <div>
+        <div> Welcome user from a beautiful part of the internet! This website is self-hosted as illustrated below.</div>
+        Please note that the frontend in itself is in it's building stage & will be beautified using React & Tailwindcss soon.<br />
+        You can check the process at this<a href="https://github.com/ShreekrishnaUdupa/Portfolio" style={{ color: 'blue' }}> Github Repo </a>
+
+        <br />
+
+        <div>
+          Contact Details: 
+          <a href="/Shreekrishna-Resume.pdf" style={{ color: 'blue' }}> Resume,  </a>
+          <a href="mailto:ShreekrishnaUdupa@gmail.com" style={{ color: 'blue' }}> Email, </a>
+          <a href="https://www.linkedin.com/in/shreekrishnaudupa/" style={{ color: 'blue' }}> LinkedIn </a>
+        </div>
+      </div>
+
+    <div>
+      <img src={DeploymentDiagram} width/>
+    </div>
     </>
   )
 }
